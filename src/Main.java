697fcec7747a7307d7911fed170de848e8e02622
Kapil <kydv901@gmail.com>
2025-06-02 16:00:55 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a Date object for the current date and time
+        Date currentDate = new Date();
+
+        // Print the current date and time
+        System.out.println("Current Date and Time: " + currentDate);
+    }
+}
